f087b730fca9c7f1cc909598fc52e0991dbcf433
Md. Zubayer Ahmad Shibly <159265436+iamshibly@users.noreply.github.com>
2025-02-27 15:05:09 +0600

    Add files via upload

---
+++ 1. Python Basics/Module 1- Introduction and The Basics of Programming in Python/3. Operators and Operands.py
@@ -0,0 +1,13 @@
+print(100+3.2)#expression 100+3.2 has a vlaue of 103.2
+print(10/8)
+print(10//8)#In Python 3, which we will be using, the division operator / produces a floating point result (even if the result is an integer; 4/2 is 2.0). If you want truncated division, which ignores the remainder, you can use the // operator (for example, 5//2 is 2).
+print(10%8)
+print(5.0/2.0)
+print(5.0/2)
+print(5.0//2.0)
+print(5/2.0)
+print(5//2.0)
+print(5//2)
+print(4 ** 2)
+print(2 ** 3 ** 2)     # the right-most ** operator gets done first!
+print((2 ** 3) ** 2)   # use parentheses to force the order you want!
