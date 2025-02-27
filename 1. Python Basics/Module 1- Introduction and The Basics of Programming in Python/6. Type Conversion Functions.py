bc7050f9617d91ddc24ae8f9b27d7ea54b74ff78
Md. Zubayer Ahmad Shibly <159265436+iamshibly@users.noreply.github.com>
2025-02-27 15:06:51 +0600

    Add files via upload

---
+++ 1. Python Basics/Module 1- Introduction and The Basics of Programming in Python/6. Type Conversion Functions.py
@@ -0,0 +1,13 @@
+print(3.14, int(4.44))## This doesn't round to the closest int!, this just ignores .44
+print(3, float(4))
+print(5, int("55"))
+print(12, int (12))
+#print(int("13Jhon"))error Jhon can not become integer
+print(float("123.45"))
+print(type(float("123.45")))
+print(str(17))
+print(str(123.45))
+print(type(str(123.45)))
+value = 50-20
+print("The answer is " + str(value))
+print("The answer is" ,value)
