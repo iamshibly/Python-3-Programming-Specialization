231f207f166571f48e1fc2a625b77cd6347183a4
Md. Zubayer Ahmad Shibly <159265436+iamshibly@users.noreply.github.com>
2025-02-27 15:05:44 +0600

    Add files via upload

---
+++ 1. Python Basics/Module 1- Introduction and The Basics of Programming in Python/4. Function Calls.py
@@ -0,0 +1,15 @@
+def square(x):
+    return x * x
+
+def sub(x,y):
+    return x-y
+
+def add(x,y):
+    return x+y
+
+print(sub)
+print(square)
+print(add)
+print(add(5,10) + 5)
+print(square(square(3)))
+print(add(square(5),sub(square(5),add(5,10))))
