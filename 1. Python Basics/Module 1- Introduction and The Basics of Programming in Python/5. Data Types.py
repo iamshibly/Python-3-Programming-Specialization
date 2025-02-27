4da805213cae64ea4a2344e2e22cd8b006ab5c9d
Md. Zubayer Ahmad Shibly <159265436+iamshibly@users.noreply.github.com>
2025-02-27 15:06:09 +0600

    Add files via upload

---
+++ 1. Python Basics/Module 1- Introduction and The Basics of Programming in Python/5. Data Types.py
@@ -0,0 +1,20 @@
+print(type("Hello, World!"))
+print(type(17))#int
+print("Hello, World")
+print(type(3.2))#float
+print(type("17"))#string
+print(type("3.2"))#string
+print(type('This is a string.'))
+print(type("And so is this."))
+print(type("""and this."""))
+print(type('''and even this...'''))
+print('''"Oh no", she exclaimed, "Ben's bike is broken!"''')
+print("""This message 
+is wrong
+one""")
+print(42,500)
+print(42500)
+print(42,",",500)
+print(42, 17, 56, 34, 11, 4.35, 32)
+print(3.4, "hello", 45)
+
