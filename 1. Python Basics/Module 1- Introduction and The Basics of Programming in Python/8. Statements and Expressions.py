a2e85cf05104c3d5abd5d52e77a16a6aa58720d5
Md. Zubayer Ahmad Shibly <159265436+iamshibly@users.noreply.github.com>
2025-02-28 15:24:50 +0600

    Add files via upload

---
+++ 1. Python Basics/Module 1- Introduction and The Basics of Programming in Python/8. Statements and Expressions.py
@@ -0,0 +1,21 @@
+print(1 + 1 + (2 * 3))
+print(len("hellooo"))
+x=5
+y=len("Bass Hoby")
+z=y+1
+print(x,y,z)
+print(5 * len("Prince")+ 7 ** len("Don"))
+def square(x):
+    return x*x
+def sub(x,y):
+    return x-y
+def add(x,y):
+    return x+y
+x = 2
+y = 1
+print(square(y + 3))
+print(square(y + square(x)))
+print(sub(square(y), square(x)))
+x=5
+y=7
+print(add(square(x),square(y)))
