906878201810639155a87bcbac8148173f25cff8
Md. Zubayer Ahmad Shibly <159265436+iamshibly@users.noreply.github.com>
2025-02-28 15:24:11 +0600

    Add files via upload

---
+++ 1. Python Basics/Module 1- Introduction and The Basics of Programming in Python/7. Variables.py
@@ -0,0 +1,24 @@
+message = "What's up, Doc?"
+n = 17
+pi = 3.14159
+print(message)
+print(n)
+print(pi)
+print(type(message))
+print(type(n))
+print(type(pi))
+x=5
+print(x)
+x=20
+print(x)
+x=6
+x=9
+print(x)
+XYZ=66
+xyz=69
+print(xyz,XYZ)
+a = 5
+b = a    # after executing this line, a and b are now equal
+print(a,b)
+a = 3    # after executing this line, a and b are no longer equal
+print(a,b)
