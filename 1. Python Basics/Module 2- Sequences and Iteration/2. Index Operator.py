1dfdd7c8ff68cd91f6775b57aba4909d299ee576
Md. Zubayer Ahmad Shibly <159265436+iamshibly@users.noreply.github.com>
2025-03-04 00:34:51 +0600

    Add files via upload

---
+++ 1. Python Basics/Module 2- Sequences and Iteration/2. Index Operator.py
@@ -0,0 +1,26 @@
+#string indexinh
+s="joy bangla"
+print(s[6])
+school = "Luther College"
+m = school[2]
+print(m)
+print(len(s))
+lastchar = school[-1]
+print(lastchar)
+print(len(lastchar))
+#list Indexing
+mylists = ["hello",50,2.5,"jhon",'ohh',[30,40],["""ola"""]]
+z= mylists[5]
+print(z)
+print(mylists[6])
+print(mylists[0])
+print(type(mylists))
+print(len(mylists))
+#tuples indexing
+mytuples1 = ("hyhy", 5, 7, (5,7),'owww',("hoe",5))
+print(mytuples1[0])
+print(mytuples1[3])
+print(mytuples1[-1])
+print(mytuples1[5-3])
+print(len(mytuples1))
+print(mytuples1[len(mytuples1)-1])
