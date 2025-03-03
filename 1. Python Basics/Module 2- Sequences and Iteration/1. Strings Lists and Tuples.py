3e1141dea8c266855e3334157aeaa52296fb14dc
Md. Zubayer Ahmad Shibly <159265436+iamshibly@users.noreply.github.com>
2025-03-04 00:29:02 +0600

    Add files via upload

---
+++ 1. Python Basics/Module 2- Sequences and Iteration/1. Strings Lists and Tuples.py
@@ -0,0 +1,30 @@
+#strings
+s= "hello sir"
+p= 'hey man'
+q= """
+hey 
+my friend 
+how are the uni"""
+n=''
+o= "60"
+print(s,p,q)
+print(n)
+print(type(o))
+print(int(o))
+
+#lists
+mylists = ["hello",50,2.5,"jhon",'ohh',[30,40],["""ola"""]]
+my2lists = [60]
+print(mylists)
+
+#tuples
+mytuples1 = ("hyhy", 5, 7, (5,7),'owww',("hoe",5))
+mytuples2=()
+mytuples3=(5,)
+print(mytuples1)
+
+
+
+print(type(o))
+print(type(my2lists))
+print(type(mytuples3 ))
