3468627eee19fd77de82de93038613df1c3107bc
Md. Zubayer Ahmad Shibly <159265436+iamshibly@users.noreply.github.com>
2025-03-06 02:34:59 +0600

    Add files via upload

---
+++ 1. Python Basics/Module 2- Sequences and Iteration/5. Concatenation and Repetition.py
@@ -0,0 +1,8 @@
+fruit = ["Mango", "Banana","Apple","Jackfruit","Guava"]
+fruit = fruit + [0,1,2,3]
+print(fruit)
+print(len(fruit))
+print(fruit+[4,5,6]+[7,8,9,10]*2)
+fruit =fruit *2
+print(fruit)
+print([0,1]*4)
