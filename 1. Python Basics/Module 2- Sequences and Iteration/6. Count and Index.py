33d7a927174822fecc6d09f41880924ef17a676a
Md. Zubayer Ahmad Shibly <159265436+iamshibly@users.noreply.github.com>
2025-03-06 02:35:21 +0600

    Add files via upload

---
+++ 1. Python Basics/Module 2- Sequences and Iteration/6. Count and Index.py
@@ -0,0 +1,16 @@
+#string
+a = "I have had an apple on my desk before!"
+print(a.count("e"))
+print(a.count(" "))
+print(a.count("ha"))
+print(a.index("e"))
+print(a.index(" "))
+print(a.index("ha"))
+#print(a.index("z")) error
+
+#list
+z = ['atoms', 4, 'neutron', 6, 'proton', 4, 'electron', 4, 'electron', 'atoms',[]]
+print(z.count(4))
+print(z.count("atoms"))
+print(z.count("o"))
+print(z.index(4))
