b4d3523f598a6b3d5ff4e47cfeac2b81d5c3a39a
Md. Zubayer Ahmad Shibly <159265436+iamshibly@users.noreply.github.com>
2025-03-05 12:36:16 +0600

    Add files via upload

---
+++ 1. Python Basics/Module 2- Sequences and Iteration/3. Length.py
@@ -0,0 +1,15 @@
+fruit = "Bananaa"
+sz = len(fruit)
+lastch = fruit[sz-1]
+print(lastch)
+mid = fruit[int((len(fruit)-1)/2)]
+# or
+midd = fruit[len(fruit)//2]
+print(midd)
+
+alist =  ["hello", 2.0, 5]
+print(len(alist))
+print(len(alist[0]))
+
+
+
