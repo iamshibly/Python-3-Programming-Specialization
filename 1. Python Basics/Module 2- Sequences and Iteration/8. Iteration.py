e041bfba00dc887ca86aec5ea4ff01b94058a19b
Md. Zubayer Ahmad Shibly <159265436+iamshibly@users.noreply.github.com>
2025-03-08 13:25:50 +0600

    Add files via upload

---
+++ 1. Python Basics/Module 2- Sequences and Iteration/8. Iteration.py
@@ -0,0 +1,27 @@
+for name in ["ABC","BDC","GHJ","DFG","HHJ","YUI","TYU"]:#loop variable or Iteration variable
+    print("Hi",name,"Please help me")
+#Strings and for loops
+for strr in "Joy Bangla Lol":
+    print(strr)
+s = "python rocks"
+for ch in s:
+   print("HELLO")
+#Lists and for loops¶
+fruit= ["abc","ghj","ase","ert"]
+for fruit in fruit:
+    print(fruit)
+    print(fruit[0])
+#turtle and loop
+import turtle
+wn = turtle.Screen()
+me= turtle.Turtle()
+for i in range(4):
+    me.forward(50)
+    me.right(90)
+
+jh = turtle.Turtle()
+for colour in ["green","red","blue","pink"]:
+    jh.color(colour)
+    jh.forward(200)
+    jh.right(90)
+wn.exitonclick()
