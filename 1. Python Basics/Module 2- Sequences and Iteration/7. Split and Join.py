7244228899669709360ca6180c6ff86b95e0759a
Md. Zubayer Ahmad Shibly <159265436+iamshibly@users.noreply.github.com>
2025-03-08 13:25:24 +0600

    Add files via upload

---
+++ 1. Python Basics/Module 2- Sequences and Iteration/7. Split and Join.py
@@ -0,0 +1,11 @@
+z= "I am doing this in nine am, not doing in nineteen pm"
+print(z.split())
+print(z.split("am"))
+print(z.split("do"))
+print(z.split("i"))
+
+p = ["wow","cow","row","sow","tow"]
+q=";"
+print(q.join(p))
+print("SEE".join(p))
+print(" ".join(p))
