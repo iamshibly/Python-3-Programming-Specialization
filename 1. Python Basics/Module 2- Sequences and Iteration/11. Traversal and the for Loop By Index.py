39b3f446829299b7ee770ef579079c4c5e842bbd
Md. Zubayer Ahmad Shibly <159265436+iamshibly@users.noreply.github.com>
2025-03-10 01:20:18 +0600

    Add files via upload

---
+++ 1. Python Basics/Module 2- Sequences and Iteration/11. Traversal and the for Loop By Index.py
@@ -0,0 +1,11 @@
+fruits = ['apple', 'pear', 'apricot', 'cherry', 'peach']
+for i in range(5):
+    print(i, fruits[i])
+
+fruits = ['apple', 'pear', 'apricot', 'cherry', 'peach']
+for n in range(len(fruits)):
+    print(n, fruits[n])
+
+fruits = ['apple', 'pear', 'apricot', 'cherry', 'peach']
+for fruit in fruits:
+    print(fruit)
