6394aa44bb2863e1e30aeae89332d1869a616134
Md. Zubayer Ahmad Shibly <159265436+iamshibly@users.noreply.github.com>
2025-03-15 10:51:22 +0600

    Add files via upload

---
+++ 1. Python Basics/Module 3- Booleans and Conditionals/7.  Omitting the else Clause and Unary Selection.py
@@ -0,0 +1,14 @@
+x = 10
+if x < 0:
+    print("The negative number ",  x, " is not valid here.")
+print("This is always printed")
+#Nested Conditionals
+x=10
+y=10
+if x>y:
+    print(x,"is greater than",y)
+else:
+    if x==y:
+        print(x,"is equal to",y)
+    else:
+        print(x,"is less than",y)
