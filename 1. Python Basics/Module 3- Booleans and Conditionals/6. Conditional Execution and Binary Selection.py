c5b60eab3d9485dbce1913d73448eefa9d1ee8e8
Md. Zubayer Ahmad Shibly <159265436+iamshibly@users.noreply.github.com>
2025-03-15 10:50:54 +0600

    Add files via upload

---
+++ 1. Python Basics/Module 3- Booleans and Conditionals/6. Conditional Execution and Binary Selection.py
@@ -0,0 +1,18 @@
+x=int(input("enter number: "))
+if x%2==0:
+    print(x,"is even")
+else:
+    print(x,"is odd")
+
+courses = ["ENGR 101", "SI 110", "ENG 125", "SI 106", "CHEM 130"]
+if "SI 106" in courses:
+    output= "You can apply to SI!"
+else:
+    output= "Take SI 106!"
+a = 20
+b = 15
+if b > a:
+    a = a * 2
+c = a + b
+
+
