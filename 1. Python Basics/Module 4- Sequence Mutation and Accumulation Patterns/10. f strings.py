7bb1e3bc44608d1777937a811f8f43045f95ca30
Md. Zubayer Ahmad Shibly <159265436+iamshibly@users.noreply.github.com>
2025-03-26 00:01:25 +0600

    Add files via upload

---
+++ 1. Python Basics/Module 4- Sequence Mutation and Accumulation Patterns/10. f strings.py
@@ -0,0 +1,13 @@
+name = "Rodney Dangerfield"
+father = "son of mine"
+score = -1.6789
+
+print("Hello {}. Your score is {}".format(name,score))
+print(f"Hello {name}. Your score is {score}")
+print(f"Hello {'Rodney Dangerfield son of mine'}. Your score is {-1.6789}")
+print(f"Hello {'Rodney Dangerfield'+' '+'son of mine'}. Your score is {-1-0.6789}")
+print(f"Hello {'Rodney Dangerfield'+' '+'son of mine'}. Your score is {max(-1.6789,-2)}")
+print(f"Hello {'Rodney Dangerfield'+' '+'son of mine'}. Your score is {max(-1.6789,-2):.2f}")
+
+
+
