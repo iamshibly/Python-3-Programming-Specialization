b4b776d86ccd0289ad5e4bf3bba48f83255997cb
Md. Zubayer Ahmad Shibly <159265436+iamshibly@users.noreply.github.com>
2025-03-25 12:02:09 +0600

    Add files via upload

---
+++ 1. Python Basics/Module 4- Sequence Mutation and Accumulation Patterns/8. Non-Mutating Methods on Strings.py
@@ -0,0 +1,17 @@
+s="YoooO suiiiiii"
+print(s)
+print(s.upper())
+print(s)
+print(s.lower())
+
+p="     josssss  zimmk      "
+a=p.count("s")
+print(a)
+print("****"+p.strip()+"****")
+print(p.strip())
+b=p.replace("s","fu")
+print(b)
+c=p.index("o")
+print(c)
+s = "python rocks"
+print(s[1]*s.index("n"))
