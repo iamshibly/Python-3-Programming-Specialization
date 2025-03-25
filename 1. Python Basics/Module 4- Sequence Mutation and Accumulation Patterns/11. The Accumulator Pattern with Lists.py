f23d12b4a63009ca8d0a8d3ecbd19f4e50d341de
Md. Zubayer Ahmad Shibly <159265436+iamshibly@users.noreply.github.com>
2025-03-26 00:01:45 +0600

    Add files via upload

---
+++ 1. Python Basics/Module 4- Sequence Mutation and Accumulation Patterns/11. The Accumulator Pattern with Lists.py
@@ -0,0 +1,18 @@
+num=[3,4,5]
+accum=[]
+for w in num:
+    x= w**2
+    accum.append(x)
+print(accum)
+
+alist=[4,2,8,6,5]
+blist=[]
+for item in alist:
+    blist.append(item+5)
+print(blist)
+
+lst=[3,0,9,4,1,7]
+new_list=[]
+for i in range(len(lst)):
+    new_list.append(lst[i]+5)
+print(new_list)
